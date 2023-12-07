b1696cdb0961cf251a2915edc094ce62645a87ff
David Mendez <saurionx2005@gmail.com>
2023-12-07 16:18:47 -0300

    agrego foto de perfil para la pantalla de edicion de proveedores

--- src/main/java/com/ServiceMatch/SM/controllers/ImageController.java
+++ src/main/java/com/ServiceMatch/SM/controllers/ImageController.java
@@ -4,6 +4,7 @@ import com.ServiceMatch.SM.entities.Image;
 import com.ServiceMatch.SM.entities.Provider;
 import com.ServiceMatch.SM.services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,10 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.io.InputStream;
+import java.nio.file.Path;
+
+import static java.nio.file.Files.readAllBytes;
+
 
 @RestController
 public class ImageController {
@@ -27,8 +31,21 @@ public class ImageController {
         final Image[] m = {new Image()};
         imageService.getProviderById(id).ifPresent(p -> m[0] = p.getImagen());
         if (m[0] == null) {
-            return ResponseEntity.notFound().build();
+            try {
+                ClassPathResource imageFile = new ClassPathResource("img/trabajador11.jpg");
+                InputStream inputStream = imageFile.getInputStream();
+
+                byte[] imageBytes = readAllBytes((Path) inputStream);
+
+                return ResponseEntity.status(HttpStatus.OK)
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(imageBytes);
+            } catch (IOException e) {
+                // Handle exception if the default image loading fails
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
         }
+
         byte[] imagenBytes = m[0].getContenido();
 
         HttpHeaders headers = new HttpHeaders();
@@ -39,19 +56,3 @@ public class ImageController {
 
     }
 }
-
-/*
-
-
-
-
-            // Convertir la imagen a un array de bytes
-            byte[] imagenBytes = imagen.getBytes();
-
-            // Crear una respuesta con el contenido de la imagen y el tipo de contenido
-
-
-
-        */
-
-
