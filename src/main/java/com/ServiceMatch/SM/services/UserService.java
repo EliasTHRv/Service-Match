1fc54c618615a9498f78fd63fb308f6ee79e1353
gpauliana <anapaulina92@gmail.com>
2023-12-08 11:19:13 -0300

    solucioné lo de los botones en la navbar según sesion, nos faltaban unas líneas en el servicio

--- src/main/java/com/ServiceMatch/SM/services/UserService.java
+++ src/main/java/com/ServiceMatch/SM/services/UserService.java
@@ -2,10 +2,14 @@ package com.ServiceMatch.SM.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -14,14 +18,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.ServiceMatch.SM.entities.AppUser;
 import com.ServiceMatch.SM.enums.RolEnum;
 import com.ServiceMatch.SM.exceptions.MyException;
 import com.ServiceMatch.SM.repository.UserRepository;
-import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -107,6 +110,39 @@ public class UserService implements UserDetailsService {
         return userRepository.findAll(pageable);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser appUser = userRepository.findByEmail(email);
+        if (appUser != null) {
+            List<GrantedAuthority> permissions = new ArrayList<>();
+            GrantedAuthority p = new SimpleGrantedAuthority("ROLE_" + appUser.getRol().toString());
+            permissions.add(p);
+            // AGREGUÉ ESTAS LINEAS PARA PODER USAR USUARIOSSESION EN EL FRONT PARA LA
+            // NAVVAR
+            ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+            HttpSession session = attr.getRequest().getSession(true);
+            session.setAttribute("usuariosession", appUser);
+            // FIN DE AGREGADO
+            return new User(appUser.getEmail(), appUser.getPassword(), permissions);
+
+        } else {
+            return null;
+        }
+    }
+
+    public AppUser getOne(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public List<AppUser> loadUserByRol(RolEnum rol) {
+        return userRepository.findByRol(rol);
+    }
+
+    public List<AppUser> loadUserBySkyll(String skill) {
+
+        return userRepository.findProvidersBySkill(skill);
+    }
+
     private void validar(String name, String email, String password, String password2)
             throws MyException {
 
@@ -130,34 +166,4 @@ public class UserService implements UserDetailsService {
 
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        AppUser appUser = userRepository.findByEmail(email);
-
-        if (appUser != null) {
-            List<GrantedAuthority> permissions = new ArrayList<>();
-
-            GrantedAuthority p = new SimpleGrantedAuthority("ROLE_" + appUser.getRol().toString());
-
-            permissions.add(p);
-            return new User(appUser.getEmail(), appUser.getPassword(), permissions);
-
-        } else {
-            return null;
-        }
-
-    }
-
-    public AppUser getOne(Long id) {
-        return userRepository.findById(id).get();
-    }
-
-    public List<AppUser> loadUserByRol(RolEnum rol) {
-        return userRepository.findByRol(rol);
-    }
-     public List<AppUser> loadUserBySkyll(String skill){
-          
-          return userRepository.findProvidersBySkill(skill);
-      }
-    
 }
